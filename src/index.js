058ca3840e21b83e63d81145916b7782b0afbeb0
unknown <polibos6366@gmail.com>
2024-09-12 17:26:59 -0600

    Validation with express-validator

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import {
+  query,
+  validationResult,
+  body,
+  matchedData,
+  checkSchema,
+} from "express-validator";
+import createUserValidationSchema from "./utils/validationSchemas.js";
 
 const app = express(); // Create an Express application
 
@@ -45,23 +53,40 @@ app.get("/", (req, res) => {
   res.status(201).send("Hello world");
 });
 
-app.get("/api/users", (req, res) => {
-  console.log(req.query);
-  const {
-    query: { filter, value },
-  } = req;
+app.get(
+  "/api/users",
+  query("filter")
+    .isString()
+    .notEmpty()
+    .withMessage("Must not be empty")
+    .isLength({ min: 3, max: 10 })
+    .withMessage("Must be at least 3-10 char"),
+  (req, res) => {
+    const result = validationResult(req);
+    console.log(result);
 
-  if (filter && value)
-    res.json(mockUsers.filter((user) => user[filter].includes(value)));
+    const {
+      query: { filter, value },
+    } = req;
 
-  return res.json(mockUsers);
-});
+    if (filter && value)
+      res.json(mockUsers.filter((user) => user[filter].includes(value)));
 
-app.post("/api/users", (req, res) => {
-  const body = req.body;
-  body.id = mockUsers.length + 1;
-  mockUsers.push(body);
-  console.log(body);
+    return res.json(mockUsers);
+  }
+);
+
+app.post("/api/users", checkSchema(createUserValidationSchema), (req, res) => {
+  const result = validationResult(req);
+  console.log(result);
+  if (!result.isEmpty()) {
+    return res.status(400).send({ errors: result.array() });
+  }
+  const data = matchedData(req);
+
+  data.id = mockUsers.length + 1;
+  mockUsers.push(data);
+  console.log(data);
 
   return res.json(mockUsers).status(201);
 });
@@ -100,21 +125,8 @@ app.patch("/api/users/:id", resolveIndexByUserId, (req, res) => {
   return res.sendStatus(200);
 });
 
-app.delete("/api/users/:id", (req, res) => {
-  const {
-    body,
-    params: { id },
-  } = req;
-
-  const parsedId = parseInt(id);
-
-  if (isNaN(parsedId)) return res.sendStatus(400);
-
-  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-
-  if (findUserIndex === -1) {
-    return res.sendStatus(404);
-  }
+app.delete("/api/users/:id", resolveIndexByUserId, (req, res) => {
+  const { findUserIndex } = req;
 
   mockUsers.splice(findUserIndex, 1);
 
