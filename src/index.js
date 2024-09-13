701b0d3cfd043bbf994bdb00a652ee39b33d7636
unknown <polibos6366@gmail.com>
2024-09-12 18:28:28 -0600

    cookies

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import usersRouter from "./routes/users.js";
 import productsRouter from "./routes/products.js";
 
+import cookieParser from "cookie-parser";
+
 const app = express(); // Create an Express application
 
 const PORT = process.env.PORT;
 
 //middlewares
 // parse json
+app.use(cookieParser());
 app.use(express.json());
 app.use(usersRouter);
 app.use(productsRouter);
@@ -21,7 +24,8 @@ app.use(loggingMiddleware);
 // Routes / Enpoints
 // Define a basic route
 app.get("/", (req, res) => {
-  res.status(201).send("Hello world");
+  res.cookie("hello", "world", { maxAge: 60000 * 60 });
+  res.status(201).json({ msg: "Hello world" });
 });
 
 // Start the server and listen on the specified port
--- src/routes/products.js
+++ src/routes/products.js
@@ -3,7 +3,13 @@ import Router from "express";
 const router = Router();
 
 router.get("/api/products", (req, res) => {
-  res.status(200).json([{ id: 1, name: "Chicke leg", price: 1.2 }]);
+  console.log(req.headers.cookie);
+  console.log(req.cookies);
+  if (req.cookies.hello && req.cookies.hello === "world") {
+    return res.status(200).json([{ id: 1, name: "Chicke leg", price: 1.2 }]);
+  }
+
+  return res.send({ msg: "Sorry you need the cookies" });
 });
 
 export default router;
